402d172e44ed1dbdc6090972257dc6fd8b472bc1
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2023-01-27 14:08:34 -0600

    fixed logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,13 +39,15 @@ exports.postLogin = (req, res, next) => {
         return next(err);
       }
       req.flash("success", { msg: "Success! You are logged in." });
-      res.redirect(req.session.returnTo || "/feed");
+      res.redirect(req.session.returnTo || "/profile");
     });
   })(req, res, next);
 };
 
 exports.logout = (req, res) => {
-  req.logout();
+  req.logout(() => {
+    console.log('User has logged out.')
+  })
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
@@ -56,7 +58,7 @@ exports.logout = (req, res) => {
 
 exports.getSignup = (req, res) => {
   if (req.user) {
-    return res.redirect("/feed");
+    return res.redirect("/profile");
   }
   res.render("signup", {
     title: "Create Account",
@@ -108,7 +110,7 @@ exports.postSignup = (req, res, next) => {
           if (err) {
             return next(err);
           }
-          res.redirect("/feed");
+          res.redirect("/profile");
         });
       });
     }
