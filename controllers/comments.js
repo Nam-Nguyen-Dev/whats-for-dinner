8756e81201a2d34952d2f47aa79e2f9af9e1c872
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2023-02-05 14:18:17 -0600

    added routes and controller for delete

--- controllers/comments.js
+++ controllers/comments.js
@@ -17,4 +17,46 @@ module.exports = {
           console.log(err);
         }
       },
+      deleteComment: async (req, res) => {
+        try {
+          // Find comment by id
+          let comment = await Comment.findById({ _id: req.params.id });
+
+          // Delete comment from db
+          await Comment.remove({ _id: req.params.id });
+          console.log("Deleted Comment");
+          res.redirect("back");
+        } catch (err) {
+          res.redirect("back");
+        }
+      },
+      getEditComment: async (req, res) => {
+        try {
+          let comment = await Comment.findById({ _id: req.params.id });
+          res.render("edit-comment.ejs", { comment: comment, user: req.user });
+        } catch (err) {
+          console.log(err);
+        }
+      },
+      editComment: async (req, res) => {
+        try {
+    
+/*           await Comment.findOneAndUpdate(
+            {_id:req.params.id},
+            {
+            title: standardizedTitle,
+            caption: req.body.caption,
+            country: req.body.country,
+            dish: req.body.dish,
+            ingredients: req.body.ingredients.trim().split('\n'),
+            directions: req.body.directions.trim().split('\n'),
+            }
+          ); */
+    
+          console.log("Post has been edited!");
+          res.redirect("back");
+        } catch (err) {
+          console.log(err);
+        }
+      },
 };
